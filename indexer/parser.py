10d1e044584bb25c107de90355c2a9c819e53230
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-04-13 22:59:57 +0200

    add separation into keywords

--- indexer/parser.py
+++ indexer/parser.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import re
 
 def is_video(filename):
     """Check if the file has one of the authorized video extensions."""
@@ -8,4 +9,13 @@ def is_video(filename):
 
 def separate_keywords(filename):
     """Separate the most pertinent keywords from a given filename."""
+    additionnal_info_re = r'([^\x00-\x7F])|(\([^\(]+\))|(\[[^\[]+\])| \
+        (\w+\d+\w+)|(\d+\w+)|(the)|(hevc)|(psa)|(bluray)|(aac)|(chs)| \
+        (tigole)'
+    separators_re = r"[\s\.-]"
+    filtered_name = re.sub(additionnal_info_re, '', filename.lower())
+    separated_words = [
+        x for x in re.split(separators_re, filtered_name)[:-1] if x != '']
+    separated_words.sort(key=len, reverse=True)
+    return separated_words[:6]
     
--- tests/units/indexer_test/test_parser.py
+++ tests/units/indexer_test/test_parser.py
@@ -1,4 +1,5 @@
 from indexer.parser import is_video
+from indexer.parser import separate_keywords
 
 def test_correct_video():
     """Test the detection of true video files."""
@@ -17,3 +18,15 @@ def test_weird_filenames():
     """Test the filtering of non-standard filenames."""
     assert is_video('true.video.with.dots.MKV') == True
     assert is_video('NoT.ViDEo.mkv.mp3') == False
+
+
+def test_separate_keywords():
+    """Test the separation of keywords."""
+    list1 = separate_keywords('My Super Movie [2019] [FR].mkv')
+    list2 = separate_keywords('the.movie.of.the.year.2018.hevc.aac.mp4')
+    list3 = separate_keywords('Python the movie 2: Return \
+        with a mega revenge (2019).mkv')
+
+    assert list1 == ['super', 'movie', 'my']
+    assert list2 == ['movie', 'year', 'of']
+    assert list3 == ['revenge', 'python', 'return', 'movie', 'with', 'mega']
