863d34c9dc1b80cbf7dd5caaefdbe2966ba798b6
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-05-04 19:11:33 +0200

    Add constructor for Movie class

--- indexer/movie.py
+++ indexer/movie.py
@@ -1,10 +1,15 @@
 """Represent the Movie class inherited from the media class."""
 
 from . import Media
-
+from indexer.parser import separate_keywords
 
 class Movie(Media):
-    
+
+
+    def __init__(self, filename):
+        """Create a movie object from its raw filename."""
+        self.filename = filename
+        self.keywords = separate_keywords(filename)    
 
     def to_dict(self):
         """Export the movie to a dict."""
