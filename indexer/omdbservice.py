e8ed4e6206c74904386c3245ab0976f326424eb9
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-05-04 19:27:07 +0200

    move omdbservice functions to a class

--- indexer/omdbservice.py
+++ indexer/omdbservice.py
@@ -4,18 +4,23 @@ import requests
 import configparser
 
 
-def get_api_key():
+def get_api_key(self):
     config = configparser.ConfigParser()
     config.read('config/config.ini')
     return config['OMDB']['api_key']
 
 
-def query_db(keywords):
-    api_key = get_api_key()
-    keywords_request = '+'.join(keywords)
-    request_url = \
-        f'http://www.omdbapi.com/?apikey={api_key}&s={keywords_request}'
-    r = requests.get(request_url)
-    return r.json()
+class QueryService():
+
+    def __init__(self):
+        self.api_key = get_api_key()
+
+    def query_db(movie):
+        keywords = movie.keywords
+        payload = '+'.join(keywords)
+        request_url = \
+            f'http://www.omdbapi.com/?apikey={self.api_key}&s={payload}'
+        r = requests.get(request_url)
+        return r.json()
     
 
